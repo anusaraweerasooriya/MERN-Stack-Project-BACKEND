29b7494b33c4e6ff656988d3a021f2e7f4923d83
anusaraweerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2022-12-16 15:47:05 +0530

    Finished adding and updating places

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ const placesRoutes = require("./routes/places-routes");
 //import users-routes
 const usersRoutes = require("./routes/users-routes");
 
+const HttpError = require("./models/http-error");
+
 const app = express();
 
 //Register Middleware
+app.use(bodyParser.json());
 app.use("/api/places", placesRoutes); // => /api/places....
 app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
 
 //error handling middleware 404 error page
 app.use((error, req, res, next) => {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,51 +1,17 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New york, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-//Registering route
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
-  if (!place) {
-    throw new HttpError("Could not find place the place id.", 404);
-  }
+router.post("/", placesControllers.createPlace);
 
-  res.json({ place }); // {place} => {place: place}
-});
+router.patch("/:pid", placesControllers.updatePlace);
 
-//Register the router
-router.get("/users/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const user = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find place for the provided id.", 404)
-    );
-  }
-
-  res.json({ user });
-});
+router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
