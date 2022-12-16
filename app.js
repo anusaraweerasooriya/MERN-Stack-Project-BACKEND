d423e2d0a639ea7405c0f8a20a5c9ae1ca0e8dd0
anusaraweerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2022-12-16 13:52:48 +0530

    Handling errors

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const bodyParser = require("body-parser");
 
 //import places-rotes
 const placesRoutes = require("./routes/places-routes");
+//import users-routes
+const usersRoutes = require("./routes/users-routes");
 
 const app = express();
 
 //Register Middleware
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes); // => /api/places....
+app.use("/api/users", usersRoutes);
+
+//error handling middleware 404 error page
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 app.listen(5000);
